efbd8436f35be7b3f9954c10fd7a040298a6da77
Jenpey Chao <60456427+JenpeyChao@users.noreply.github.com>
2024-05-21 00:54:32 -0400

    Update DifficultyLevels.java

--- DifficultyLevels.java
+++ DifficultyLevels.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 public class DifficultyLevels {
     public static void guessing(int endRange, int countLimit){
-        
+        //creates the variables needed 
         Random rand = new Random();
         int rand_int = rand.nextInt(endRange);
         rand_int+=1;
@@ -10,9 +10,9 @@ public class DifficultyLevels {
         int attempts = 0;
         int guess = Integer.MAX_VALUE;
         Scanner myObj = new Scanner(System.in);
-
+        //loops until the user found the number or when the attempt is lower than 0
         while(guess != rand_int && countLimit > 0){
-
+            //gets the number from the user and tells if its higher or lower
             intGuess = myObj.nextLine();
             guess = Integer.parseInt(intGuess);
             if (guess < rand_int){
@@ -23,9 +23,10 @@ public class DifficultyLevels {
 
             countLimit-=1;
             attempts+=1;
-
+            //prints if you found the number
             if (guess == rand_int){
                 System.out.println("Nice you guessed the number");
+            //tells you howm any attemps you did when you ran out of attempts
             }else if (countLimit<=0){
                 System.out.println("Sorry you didnt guess the number");
                 System.out.println(attempts+" attempts");
@@ -44,7 +45,8 @@ public class DifficultyLevels {
             String diff = myObj.nextLine();
             diff.toLowerCase(); // makes it all lowercase
 
-            switch(diff){ 
+            switch(diff){ //lets the user pick if they want easy, med, or hard 
+                //each case has a limit of attempts 
                 case "easy":
                     System.out.println("You have a limit of 10 attempts");
                     System.out.println("Guess a random number between 1 and 50");
@@ -64,6 +66,7 @@ public class DifficultyLevels {
                     break;
             }
             //if they chose none of the options or misspelled they can try again
+            //allows the user to play again if they wanted to 
             System.out.println("Would you like to try again?");
             String again = myObj.nextLine();
             again.toLowerCase();
